b207ac142ed77128463f66232d7d5fe3113809a1
Emir Bolat <emirybolat@hotmail.com>
2024-11-18 12:34:43 +0300

    Initial Commit

---
+++ Persons/PersonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsApp.swift
+//  Persons
+//
+//  Created by Emir Bolat on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PersonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
